c4587aece471db2f224afcf688b66054f8e6c8cf
Phil Fei Huang <philfeihuang@Phils-MacBook-M1-Pro.local>
2022-05-24 20:01:21 -0400

    fixed bug in spot/vol marginal plots

--- sim_corr_rand/corr_rand_variables.py
+++ sim_corr_rand/corr_rand_variables.py
@@ -10,26 +10,38 @@ from skewstudent import SkewStudent
 NSCEN = 10000
 
 class CorrRandVariables2D(ABC):
-    ''' Simulated Correlated 2D Random Variables '''
+    """ Simulated Correlated 2D Random Variables """
     def __init__(self, rand_name1='Marginal1', rand_name2='Marginal2'):
+        """
+        Parameters
+        ----------
+        rand_name1: str
+            name of random number 1
+        rand_name2: str
+            name of random number 2
+        """
         self.rand_name1 = f'{rand_name1} RV'
         self.rand_name2 = f'{rand_name2} RV'
 
     @abstractmethod
     def gen_rand_num1(self):
-        ''' Marginal Distribution 1 '''
+        """ Marginal Distribution 1 """
         raise NotImplementedError
 
     @abstractmethod
     def gen_rand_num2(self):
-        ''' Marginal Distribution 2 '''
+        """ Marginal Distribution 2 """
         raise NotImplementedError
 
-    def gen_corr_rand(self, rho):
-        ''' Generate Correlated Random Numbers: Corr RV = L x RV'''
-        assert -1 <= rho <= 1, 'Correlation shall be [-1, 1]'
+    def calc_marginals(self):
+        rv1 = self.gen_rand_num1()
+        rv2 = self.gen_rand_num2()
+        self.marginals = np.stack([rv1, rv2], axis=0)
+
+    def calc_corr_rand(self, rho):
+        """ Generate Correlated Random Numbers: Corr RV = L x RV"""
+        assert -1 < rho < 1, 'Correlation shall be (-1, 1)'
         self.rho = rho
-        self.marginals = np.stack([self.rv1, self.rv2], axis=0)
         covar_mat = np.array([[1, self.rho], [self.rho, 1]])
         try:
             self.chol = cholesky(covar_mat, lower=True)
@@ -51,6 +63,9 @@ class CorrRandVariables2D(ABC):
         # plt.show()
 
     def plot_joint_scatter(self, ax=None):
+        if not hasattr(self, 'corr_rands'):
+            print('correlated RVs not calculated yet')
+            return
         if ax is None:
             fig, ax = plt.subplots(figsize=(8, 8))
         sns.scatterplot(x=self.corr_rands[self.rand_name1], y=self.corr_rands[self.rand_name2], ax=ax)
@@ -63,12 +78,10 @@ class CorrRandVariables2D(ABC):
         plt.show()
 
 class CorrBiNormT2D(CorrRandVariables2D):
-    ''' Simulate 2D Correlated Normal and Hanson Skew-T Random Numbers'''
+    """ Simulate 2D Correlated Normal and Hanson Skew-T Random Numbers"""
     def __init__(self):
         super().__init__('Normal 1', 'Normal 2')
-        self.rv1 = self.gen_rand_num1()
-        self.rv2 = self.gen_rand_num2()
-        self.gen_corr_rand()
+        self.calc_marginals()
 
     def gen_rand_num1(self):
         return np.random.normal(size=NSCEN)
@@ -77,11 +90,10 @@ class CorrBiNormT2D(CorrRandVariables2D):
         return np.random.normal(size=NSCEN)
 
 class CorrNormSkewT2D(CorrRandVariables2D):
-    ''' Simulate 2D Correlated Normal and Hanson Skew-T Random Numbers'''
+    """ Simulate 2D Correlated Normal and Hanson Skew-T Random Numbers"""
     def __init__(self, eta=4, lam=0.15):
         super().__init__('Normal', 'Hanson Skew-T')
-        self.rv1 = self.gen_rand_num1()
-        self.rv2 = self.gen_rand_num2(eta=eta, lam=lam)
+        self.calc_marginals(eta, lam)
 
     def gen_rand_num1(self):
         return np.random.normal(size=NSCEN)
@@ -90,21 +102,24 @@ class CorrNormSkewT2D(CorrRandVariables2D):
         skewt = SkewStudent(eta=eta, lam=lam)
         return skewt.rvs(NSCEN)
 
+    def calc_marginals(self, eta, lam):
+        rv1 = self.gen_rand_num1()
+        rv2 = self.gen_rand_num2(eta=eta, lam=lam)
+        self.marginals = np.stack([rv1, rv2], axis=0)
+
 def main():
     fig, axes = plt.subplots(2, 2, figsize=(12, 8), constrained_layout=True)
     rhos = [-0.2, -0.4, -0.6, -0.8]
     rho, eta, lam = -0.2, 4, 0.15
     corr_rv = CorrNormSkewT2D(eta=eta, lam=lam)
     for rho, ax in zip(rhos, axes.flatten()):
-        corr_rv.gen_corr_rand(rho=rho)
+        corr_rv.calc_corr_rand(rho=rho)
         corr_rv.plot_joint_scatter(ax=ax)
     corr_rv.plot_marginal_hist()
     plt.show()
 
 if __name__ == '__main__':
     main()
-    # rv = CorrBiNormT2D(rho=-0.8)
-    # rv.plot_marginal_joint()
 
 
 
--- sim_corr_rand/corr_spot_vol.py
+++ sim_corr_rand/corr_spot_vol.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 
 import matplotlib.pyplot as plt
+import seaborn as sns
 from corr_rand_variables import CorrBiNormT2D, CorrNormSkewT2D
 
 NSCEN = 10000
@@ -10,25 +11,50 @@ class CorrSpotVol:
     """ Simulate Correlated Log-returns of Spot and IV """
 
     def __init__(self, iv, vov, eta=4, lam=0.15):
+        """
+        Parameters
+        ----------
+        iv: implied vol
+        vov: vov-of-vol
+        eta: degree of freedom for Hanson
+        lam: skewness for Hanson
+        """
         self.corr_rv = CorrNormSkewT2D(eta=eta, lam=lam)
         self.iv = iv
         self.vov = vov
-        self.corr_rv.rand_name1 = 'Spot Return'
-        self.corr_rv.rand_name2 = 'IV Return'
+        self.rand_name1 = 'Spot Return'
+        self.rand_name2 = 'IV Return'
         self.spot_vol_return = pd.DataFrame()
 
-    def gen_corr_spot_vol(self, rho=-0.8):
-        self.corr_rv.gen_corr_rand(rho)
+    def calc_corr_spot_vol(self, rho=-0.8):
+        """ calculate spot and vol returns
+        Parameters
+        ----------
+        rho: float
+            linear correlation number
+        Returns
+        -------
+        None
+        """
+        self.rho = rho
+        self.corr_rv.calc_corr_rand(rho)
         scalar = np.array([self.iv, self.vov]) / np.sqrt(252)  # Scale unit-variance correlated RV with IV and VoV
-        self.corr_rv.corr_rands *= np.tile(scalar, (NSCEN, 1))
-        self.spot_vol_return = np.exp(self.corr_rv.corr_rands) - 1  # convert log-return to proportional return
+        log_retn = self.corr_rv.corr_rands * np.tile(scalar, (NSCEN, 1))
+        self.spot_vol_return = np.exp(log_retn) - 1  # convert log-return to proportional return
+        self.spot_vol_return.columns = [self.rand_name1, self.rand_name2]
 
     def corr_spot_vol_stats(self):
         print('Statistical Summary of Simulated Spot and Vol Returns \n')
         print(self.spot_vol_return.describe(), '\n', self.spot_vol_return.corr())
 
-    def plot_spot_vol_scatter(self):
-        self.corr_rv.plot_marginal_joint()
+    def plot_spot_vol_scatter(self, ax=None):
+        if not hasattr(self, 'spot_vol_return'):
+            print('correlated spot/vol return not calculated yet')
+            return
+        if ax is None:
+            fig, ax = plt.subplots(figsize=(8, 8))
+        sns.scatterplot(x=self.spot_vol_return[self.rand_name1], y=self.spot_vol_return[self.rand_name2], ax=ax)
+        ax.set(xlabel=self.rand_name1, ylabel=self.rand_name2, title=f'Correlation = {self.rho}')
 
 def main():
     fig, axes = plt.subplots(2, 2, figsize=(12, 8), constrained_layout=True)
@@ -37,11 +63,10 @@ def main():
     rho, eta, lam = -0.9, 4, 0.15
     csv = CorrSpotVol(iv=iv, vov=vov, eta=eta, lam=lam)
     for rho, ax in zip(rhos, axes.flatten()):
-        csv.gen_corr_spot_vol(rho=rho)
-        csv.corr_rv.plot_joint_scatter(ax=ax)
+        csv.calc_corr_spot_vol(rho=rho)
+        csv.plot_spot_vol_scatter(ax=ax)
     csv.corr_rv.plot_marginal_hist()
     plt.show()
-
     print(csv.corr_spot_vol_stats())
 
 if __name__ == '__main__':
